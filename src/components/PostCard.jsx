fcfdb0e9314a87fabcc45d70195372fc7e30271c
KRISH KUMAR SINGH <ks092713@gmail.com>
2026-01-26 20:08:59 +0000

    fix: correct variable name from featuredImage to featureImage in PostCard and Post components

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import appwriteService from "../appwrite/config"
 import { Link } from 'react-router-dom'
 
-function PostCard({ $id, title, featuredImage }) {
+function PostCard({ $id, title, featureImage }) {
 
 //   // ✅ console.log yahan aata hai
-//   console.log("FEATURED IMAGE ID:", featuredImage)
+//   console.log("FEATURED IMAGE ID:", featureImage)
 
   return (
     <Link to={`/post/${$id}`}>
       <div className='w-full bg-gray-100 rounded-xl p-4'>
 
         {/* ✅ guard lagaya */}
-        {featuredImage && (
+        {featureImage && (
           <div className='w-full justify-center mb-4'>
             <img
-              src={appwriteService.getFilePreview(featuredImage)}
+              src={appwriteService.getFilePreview(featureImage)}
               alt={title}
               className='rounded-xl'
             />
